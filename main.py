0751267376cf38e33c0d4a8b8f11d6fb9352b9a9
Eric Rincon <eric.rincon147@gmail.com>
2017-12-04 11:04:10 -0600

    added image generator

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ from train import train_model
 from keras.preprocessing.image import ImageDataGenerator
 from keras.backend import K
 
-def main():
-    argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument('--model')
-    argument_parser.add_argument('--data')
-    args = argument_parser.parse_args()
 
+def build_data_generator(data_dir, image_shape, batch_size):
+    """
+
+    :param data_dir:
+    :param image_shape:
+    :param batch_size:
+    :return:
+    """
     # from https://keras.io/preprocessing/image/
-    image_generator = ImageDataGenerator(
+    data_generator = ImageDataGenerator(
         featurewise_center=False,
         samplewise_center=False,
         featurewise_std_normalization=False,
@@ -34,11 +37,46 @@ def main():
         preprocessing_function=None,
         data_format=K.image_data_format())
 
-    image_generator.flow_from_directory(args.data)
+
+    generator = data_generator.flow_from_directory(
+        data_dir,
+        target_size=image_shape,
+        batch_size=batch_size,
+        class_mode='categorical')
+
+    return generator
+
+
+def main():
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument('--model')
+    argument_parser.add_argument('--train-data')
+    argument_parser.add_argument('--test=data')
+    argument_parser.add_argument('--height', default=256, type=int)
+    argument_parser.add_argument('--width', default=256, type=int)
+    argument_parser.add_argument('--batch_size', default=32, type=int)
+    argument_parser.add_argument('--image_shape', default='256,256', type=str)
+    argument_parser.add_argument('--epochs')
+
+    args = argument_parser.parse_args()
+
+    image_shape = [int(x) for x in args.image_shape.split(',')]
+
+    model = build_model(args.model_name, (args.width, args.height))
+
+    train_generator = build_data_generator(args.train_data, image_shape, args.batch_size)
+    valid_generator = build_data_generator(args.valid_data, image_shape, args.batch_size)
+
+    model.fit_generator(
+        train_generator,
+        steps_per_epoch=2000,
+        epochs=50,
+        validation_data=valid_generator,
+        validation_steps=800)
 
 
 
-    # model = build_model(args.model_name, )
+
 
 
 
--- models.py
+++ models.py
@@ -2,6 +2,45 @@ from keras.models import Model
 from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense
 
 
+def vggnet(model_input):
+    """
+
+    :param model_input:
+    :return:
+    """
+
+    def build_conv_layer(layer_input, nb_convs, nb_filters, kernel_size, strides=(1, 1)):
+        """
+
+        :param layer_input:
+        :param nb_filters:
+        :param kernel_size:
+        :param strides:
+        :return:
+        """
+        convolution = layer_input
+
+        for i in range(nb_convs):
+            convolution = Conv2D(filters=nb_filters, kernel_size=kernel_size,
+                                strides=strides, activation='relu')(convolution)
+        pooling = MaxPooling2D()(convolution)
+
+        return pooling
+
+    layer_one = build_conv_layer(model_input, 2, 64, (3,3))
+    layer_two = build_conv_layer(layer_one, 2, 128, (3, 3))
+    layer_three = build_conv_layer(layer_two, 2, 256, (3,3))
+    layer_four = build_conv_layer(layer_three, 512, 3, (3, 3))
+    layer_five = build_conv_layer(layer_four, 512, 3, (3, 3))
+
+    # Fully connected layers
+    flatten = Flatten()(layer_five)
+    dense_one = Dense(4096)(flatten)
+    dense_two = Dense(4096)(dense_one)
+
+    return dense_two
+
+
 def lenet(model_input):
     """
 
@@ -18,10 +57,9 @@ def lenet(model_input):
 
         convolution = Conv2D(filters=nb_filters, kernel_size=kernel_size,
                              strides=strides, activation='relu')(layer_input)
-        pooling = MaxPooling2D()(convolution)
+        pooling = MaxPooling2D(pool_size=(2, 2))(convolution)
 
         return pooling
-
 
     layer_one = build_conv_layer(model_input, 50, 3)
     layer_two = build_conv_layer(layer_one, 50, 3)
@@ -32,20 +70,21 @@ def lenet(model_input):
 
 
 _MODELS = {
-    'lenet': lenet
+    'lenet': lenet,
+    'vggnet': vggnet
+
 }
 
 
-def _build_model(model, model_shape, loss='categorical_crossentropy',
+def _build_model(model, input_shape, loss='categorical_crossentropy',
                  optimizer='adam'):
-    model_input = Input(shape=model_shape)
+    model_input = Input(shape=input_shape)
     model_output = model(model_input)
 
     model = Model(inputs=model_input, outputs=model_output)
     model.compile(optimizer=optimizer, loss=loss)
 
     return model
-
 
 
 
